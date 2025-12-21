ee1db49be6a4d4c57ac0024ffc024ee0d0e4bf69
Prayag-waje <prayagwaje21@gmail.com>
2025-12-21 19:21:06 +0530

    login and sinup page added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import AllBlogs from './views/AllBlogs'
 import NewBlogs from './views/NewBlogs'
 import EditBlogs from './views/EditBlogs'
 import ReadBlogs from './views/ReadBlogs'
+import Login from './views/Login'
+import Signup from './views/Signup'
 
 createRoot(document.getElementById('root')).render(
   <browserRouter>
@@ -13,6 +15,8 @@ createRoot(document.getElementById('root')).render(
       <Route path = "/new" element = {<NewBlogs />}/>
       <Route path = "/edit/:id" element = {<EditBlogs />}/>
       <Route path = "/blog/:id" element = {<ReadBlogs />}/>
+      <Route path = "/login" element = {<Login />}/>
+      <Route path = "/signup" element = {<Signup />}/>
       <Route path = "*" element = {<div><h1>404 Not Found</h1></div>} />
     </Routes>
   </browserRouter>
